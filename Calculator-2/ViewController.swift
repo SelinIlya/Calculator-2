5b8ff1846ac950b113151daecd034685f5d532c4
Selin Ilya <IVSelin@mail.ru>
2022-04-16 18:48:35 +0400

    Inin commit

--- Calculator-2/ViewController.swift
+++ Calculator-2/ViewController.swift
@@ -8,12 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    private var isFinishedTypingNumber : Bool = true
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    @IBAction func numButtonPressed(_ sender: UIButton) {
+        isFinishedTypingNumber = true
+        let number = Double(displayLabel.text!)!
+        
+        if let calcMethod = sender.currentTitle {
+            if calcMethod == "+/-" {
+                displayLabel.text = String(number * -1)
+            } else if calcMethod == "AC" {
+                displayLabel.text = "0"
+            } else if calcMethod == "%" {
+                displayLabel.text = String(number / 100)
+            }
+        }
     }
-
-
+    
+    
+    @IBAction func calcButtonPressed(_ sender: UIButton) {
+        if let numValue = sender.currentTitle {
+            
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text?.append(numValue)
+            }
+            
+        }
+    }
 }
 
